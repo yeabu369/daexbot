077dadcf28818a29d9c4f5b9965593b3332099f6
yeabu369 <yeabsdebe@gmail.com>
2022-09-08 08:07:31 +0000

    Changed Text Button to Web App button

--- src/keyboards/select-action.keyboard.ts
+++ src/keyboards/select-action.keyboard.ts
@@ -1,3 +1,6 @@
 import { Keyboard } from "grammy";
 
-export const keyboard = new Keyboard().text("Place Order");
+export const keyboard = new Keyboard().webApp(
+  "Place Order",
+  "https://p2p-gamma.vercel.app/place-order"
+);
